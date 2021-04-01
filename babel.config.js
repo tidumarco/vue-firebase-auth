0855b4988d117e453443cd691978193d4279dda5
Marco Tidu <marcotidu@kamk.fi>
2021-04-01 22:44:59 +0300

    assignment done

--- babel.config.js
+++ babel.config.js
@@ -3,3 +3,15 @@ module.exports = {
     '@vue/cli-plugin-babel/preset'
   ]
 }
+
+/*require('@babel/register')({
+	extends: './.babelrc',
+	ignore: [/node_modules/],
+	extensions: ['.js', '.jsx', 'ts', '.tsx'],
+});
+
+require('core-js/es5');
+require('core-js/es6');
+require('core-js/library');
+require('core-js/stage/0');
+require('regenerator-runtime/runtime');*/
--- src/main.js
+++ src/main.js
@@ -1,4 +1,27 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import * as firebase from 'firebase';
 
-createApp(App).mount('#app')
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@/assets/css/main.css'
+
+Vue.config.productionTip = false
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAeqvoSZWs8FyGbufsEsrrV3IcnsVFue8M",
+    authDomain: "vie-firebase-auth.firebaseapp.com",
+    projectId: "vie-firebase-auth",
+    storageBucket: "vie-firebase-auth.appspot.com",
+    messagingSenderId: "51948497371",
+    appId: "1:51948497371:web:0ebd850e2ab18587ae9ffe",
+    measurementId: "G-MV3FDJT09Z"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
